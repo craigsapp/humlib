5bc6f7d129c012eb09a3b92f3fee889681b7cec8
Craig Sapp <craigsapp@gmail.com>
2016-06-18 22:37:56 -0700

    Fixed flats' values extracted from key signature.

--- src/HumdrumFileContent-accidental.cpp
+++ src/HumdrumFileContent-accidental.cpp
@@ -219,20 +219,20 @@ bool HumdrumFileContent::analyzeKernAccidentals(void) {
 void HumdrumFileContent::fillKeySignature(vector<int>& states,
 		const string& keysig) {
 	std::fill(states.begin(), states.end(), 0);
-	if (keysig.find("f#") != string::npos) { states[3] = 1; }
-	if (keysig.find("c#") != string::npos) { states[0] = 1; }
-	if (keysig.find("g#") != string::npos) { states[4] = 1; }
-	if (keysig.find("d#") != string::npos) { states[1] = 1; }
-	if (keysig.find("a#") != string::npos) { states[5] = 1; }
-	if (keysig.find("e#") != string::npos) { states[2] = 1; }
-	if (keysig.find("b#") != string::npos) { states[6] = 1; }
-	if (keysig.find("b-") != string::npos) { states[6] = 1; }
-	if (keysig.find("e-") != string::npos) { states[2] = 1; }
-	if (keysig.find("a-") != string::npos) { states[5] = 1; }
-	if (keysig.find("d-") != string::npos) { states[1] = 1; }
-	if (keysig.find("g-") != string::npos) { states[4] = 1; }
-	if (keysig.find("c-") != string::npos) { states[0] = 1; }
-	if (keysig.find("f-") != string::npos) { states[3] = 1; }
+	if (keysig.find("f#") != string::npos) { states[3] = +1; }
+	if (keysig.find("c#") != string::npos) { states[0] = +1; }
+	if (keysig.find("g#") != string::npos) { states[4] = +1; }
+	if (keysig.find("d#") != string::npos) { states[1] = +1; }
+	if (keysig.find("a#") != string::npos) { states[5] = +1; }
+	if (keysig.find("e#") != string::npos) { states[2] = +1; }
+	if (keysig.find("b#") != string::npos) { states[6] = +1; }
+	if (keysig.find("b-") != string::npos) { states[6] = -1; }
+	if (keysig.find("e-") != string::npos) { states[2] = -1; }
+	if (keysig.find("a-") != string::npos) { states[5] = -1; }
+	if (keysig.find("d-") != string::npos) { states[1] = -1; }
+	if (keysig.find("g-") != string::npos) { states[4] = -1; }
+	if (keysig.find("c-") != string::npos) { states[0] = -1; }
+	if (keysig.find("f-") != string::npos) { states[3] = -1; }
 }
 
 
