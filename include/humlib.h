d3bfc9f7552813aeb3bbd7f1b765664f340f216f
Craig Sapp <craigsapp@gmail.com>
2016-12-15 21:26:33 -0800

    Removed compiler warnings.

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Thu Dec 15 21:17:12 PST 2016
+// Last Modified: Thu Dec 15 21:23:53 PST 2016
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
