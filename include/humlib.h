bdab6ba71cd968e50a6d6c0188a83fced160bc31
Craig Sapp <craigsapp@gmail.com>
2016-12-25 17:26:27 -0800

    Update.

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Sun Dec 25 16:12:28 PST 2016
+// Last Modified: Sun Dec 25 17:13:59 PST 2016
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
