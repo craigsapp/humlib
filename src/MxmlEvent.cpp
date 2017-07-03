bf6f015f960d96840c2bb7c61b2aea0288096866
Craig Sapp <craigsapp@gmail.com>
2017-07-03 09:16:56 +0200

    Added stem direction preservation on voices above 2 on a staff.

--- src/MxmlEvent.cpp
+++ src/MxmlEvent.cpp
@@ -1386,7 +1386,7 @@ string MxmlEvent::getPostfixNoteInfo(bool primarynote) const {
 				hookbacks++;
 			}
 		} else if (nodeType(child, "stem")) {
-			if (m_stems || (m_voiceindex > 2) || (getDuration() == 0)) {
+			if (m_stems || (getVoiceIndex() >= 2) || (getDuration() == 0)) {
 				const char* stemdir = child.child_value();
 				if (strcmp(stemdir, "up") == 0) {
 					stem = 1;
