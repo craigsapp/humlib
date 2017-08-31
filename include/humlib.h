c04aac9c14176bc3053c4a77ae4b3b566455c0b3
Craig Sapp <craigsapp@gmail.com>
2017-08-30 18:11:33 -0700

    Updated composite file.

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Wed, Aug 30, 2017  3:25:45 AM
+// Last Modified: Wed Aug 30 18:09:54 PDT 2017
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
--- src/humlib.cpp
+++ src/humlib.cpp
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Wed, Aug 30, 2017  3:25:45 AM
+// Last Modified: Wed Aug 30 18:09:54 PDT 2017
 // Filename:      /include/humlib.cpp
 // URL:           https://github.com/craigsapp/humlib/blob/master/src/humlib.cpp
 // Syntax:        C++11
