fe6ef21e74c78be34e5b6fef9252c924a8161a33
Craig Sapp <craigsapp@gmail.com>
2019-06-14 15:18:56 +0200

    Add spinetract2 cli.

---
+++ cli/spinetrace2.cpp
@@ -0,0 +1,18 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Fri Jun 14 12:17:53 CEST 2019
+// Last Modified: Fri Jun 14 12:17:56 CEST 2019
+// Filename:      spinetrace.cpp
+// URL:           https://github.com/craigsapp/humlib/blob/master/tools/spinetrace.cpp
+// Syntax:        C++11
+// vim:           ts=3 noexpandtab nowrap
+//
+// Description:   Add spacing tabs between primary spines to align them.
+//
+
+#include "humlib.h"
+
+STREAM_INTERFACE(Tool_spinetrace)
+
+
+
