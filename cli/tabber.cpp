727a47cbc545d161bed17300ffd451f0e7ecf90d
Craig Sapp <craigsapp@gmail.com>
2019-06-19 12:01:01 +0200

    Add tabber cli.

---
+++ cli/tabber.cpp
@@ -0,0 +1,18 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Wed Jun 12 12:41:59 CEST 2019
+// Last Modified: Wed Jun 12 12:42:02 CEST 2019
+// Filename:      tabber.cpp
+// URL:           https://github.com/craigsapp/humlib/blob/master/tools/tabber.cpp
+// Syntax:        C++11
+// vim:           ts=3 noexpandtab nowrap
+//
+// Description:   Add spacing tabs between primary spines to align them.
+//
+
+#include "humlib.h"
+
+STREAM_INTERFACE(Tool_tabber)
+
+
+
--- src/humlib.cpp
+++ src/humlib.cpp
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Mon Jun 17 18:29:46 CEST 2019
+// Last Modified: Wed Jun 19 11:59:05 CEST 2019
 // Filename:      /include/humlib.cpp
 // URL:           https://github.com/craigsapp/humlib/blob/master/src/humlib.cpp
 // Syntax:        C++11
