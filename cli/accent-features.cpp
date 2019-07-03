d10cdefadf2a4e3c7e6977a844acad3ca8a7a38d
Craig Sapp <craigsapp@gmail.com>
2019-07-03 09:41:56 +0200

    Update accent feature extraction.

--- cli/accent-features.cpp
+++ cli/accent-features.cpp
@@ -11,6 +11,7 @@
 
 #include "humlib.h"
 #include <iostream>
+#include <ctype.h>
 
 using namespace hum;
 using namespace std;
@@ -23,9 +24,14 @@ class AccentFeatures {
 		HTp  nextNote   = NULL;
 		HTp  prevNote   = NULL;
 
+		int staff_num    = 0;
 		int chord_num   = 0;
 
 		bool accent     = false;
+		bool marcato    = false;
+		bool sforzando  = false;
+		bool tenuto     = false;
+		bool staccato   = false;
 		bool slur_start = false;
 		bool slur_end   = false;
 		bool trill      = false;
@@ -33,11 +39,35 @@ class AccentFeatures {
 		double metpos   = 0;
 };
 
-void   extractNotes       (vector<AccentFeatures>& data, HumdrumFile& infile, Options& options);
-void   extractFeatures    (vector<AccentFeatures>& data);
-void   printData          (vector<AccentFeatures>& data);
-HTp    getPreviousNote    (HTp starting);
-HTp    getNextNote        (HTp starting);
+
+class MeasureNumberInfo {
+	public:
+		int number = -1;
+		double start_time = -1.0;
+		double stop_time = -1.0;
+};
+
+
+class PartInfo {
+	public:
+		int track     = -1;
+		int staff_num = -1;
+		HTp name      = NULL;            // name of instrument such as *I"Clarinet in Bb
+		HTp abbr      = NULL;            // abbreviation of such as *I'cl
+		HTp code      = NULL;            // instrument code such as *Iclars (soprano clarinet)
+};
+
+
+
+void   extractNotes             (vector<AccentFeatures>& data, HumdrumFile& infile, Options& options);
+void   extractFeatures          (vector<AccentFeatures>& data);
+void   printData                (vector<AccentFeatures>& data, Options& options);
+HTp    getPreviousNote          (HTp starting);
+HTp    getNextNote              (HTp starting);
+void   printMeasureNumberInfo   (vector<MeasureNumberInfo>& measures);
+void   extractMeasureNumberInfo (vector<MeasureNumberInfo>& measures, HumdrumFile& infile);
+void   printPartInfo            (vector<PartInfo>& parts, HumdrumFile& infile);
+void   extractPartInfo          (vector<PartInfo>& parts, HumdrumFile& infile);
 
 
 ///////////////////////////////////////////////////////////////////////////
@@ -45,16 +75,232 @@ HTp    getNextNote        (HTp starting);
 int main(int argc, char** argv) {
 	Options options;
 	options.define("A|all=b", "extract all features");
+	options.define("k|kern=b", "include original kern notes in output feature list");
+	options.define("m|measures=b", "extract measure timings");
+	options.define("p|part-list=b", "extract part list");
 	options.process(argc, argv);
+
+	bool measuresQ = options.getBoolean("measures");
+	bool partsQ    = options.getBoolean("part-list");
+
 	HumdrumFileStream instream(options);
 	HumdrumFile infile;
 	vector<AccentFeatures> data;
+	vector<MeasureNumberInfo> measures;
+	vector<PartInfo> parts;
 	while (instream.read(infile)) {
-		extractNotes(data, infile, options);
-		extractFeatures(data);
-		printData(data);
+		if (measuresQ) {
+			extractMeasureNumberInfo(measures, infile);
+			printMeasureNumberInfo(measures);
+		} if (partsQ) {
+			extractPartInfo(parts, infile);
+			printPartInfo(parts, infile);
+		} else {
+			extractNotes(data, infile, options);
+			extractFeatures(data);
+			printData(data, options);
+		}
 	}
 	return 0;
+}
+
+
+
+//////////////////////////////
+//
+// extractPartInfo --
+//
+
+void extractPartInfo(vector<PartInfo>& parts, HumdrumFile& infile) {
+
+	// partNumber: Part number (actually staff number), with "1" being the top
+	// staff in the system.
+	vector<HTp> kernspines = infile.getKernSpineStartList();
+	int trackcount = infile.getTrackCount();
+	vector<int> partNumber(trackcount + 1, 0);
+	for (int i=0; i<(int)kernspines.size(); i++) {
+		int track = kernspines[i]->getTrack();
+		int ktrack = (int)kernspines.size() - i;
+		partNumber[track] = ktrack;
+	}
+
+	parts.clear();
+	parts.reserve(kernspines.size());
+
+	HTp current;
+	for (int i=(int)kernspines.size() - 1; i>=0; i--) {
+		PartInfo pinfo;
+		pinfo.track = kernspines[i]->getTrack();
+		pinfo.staff_num = partNumber[pinfo.track];
+		pinfo.code = NULL;
+		pinfo.abbr = NULL;
+		pinfo.name = NULL;
+		current = kernspines[i]->getNextToken();
+		while (current) {
+			if (current->isData()) {
+				break;
+			}
+			if (!current->isInterpretation()) {
+				current = current->getNextToken();	
+				continue;
+			}
+			if (current->compare(0, 3, "*I\"") == 0) {
+				pinfo.name = current;
+			} else if (current->compare(0, 3, "*I\'") == 0) {
+				pinfo.abbr = current;
+			} else if (current->compare(0, 2, "*I") == 0) {
+				string value = current->substr(2);
+				if (value.size() > 0) {
+					if (islower(value[0])) {
+						pinfo.code = current;
+					}
+				}
+			}
+			current = current->getNextToken();
+		}
+		parts.push_back(pinfo);
+	}
+}
+
+
+
+//////////////////////////////
+//
+// printPartInfo --
+//
+
+
+void printPartInfo(vector<PartInfo>& parts, HumdrumFile& infile) {
+
+	// partNumber: Part number (actually staff number), with "1" being the top
+	// staff in the system.
+	vector<HTp> kernspines = infile.getKernSpineStartList();
+	int trackcount = infile.getTrackCount();
+	vector<int> partNumber(trackcount + 1, 0);
+	for (int i=0; i<(int)kernspines.size(); i++) {
+		int track = kernspines[i]->getTrack();
+		int ktrack = (int)kernspines.size() - i;
+		partNumber[track] = ktrack;
+	}
+
+	int pfeatures = 5;
+	cout << "**track";
+	cout << "\t**staff";
+	cout << "\t**code";
+	cout << "\t**abbr";
+	cout << "\t**name";
+	cout << endl;
+
+	for (int i=(int)kernspines.size() - 1; i>=0; i--) {
+		int track = kernspines[i]->getTrack();
+		cout << track;
+		cout << "\t";
+
+		cout << partNumber[track];
+		cout << "\t";
+
+		int partindex = partNumber[track] - 1;
+		if (parts[partindex].code) {
+			cout << parts[partindex].code->substr(2);
+		} else {
+			cout << ".";
+		}
+		cout << "\t";
+
+		if (parts[partindex].abbr) {
+			cout << parts[partindex].abbr->substr(3);
+		} else {
+			cout << ".";
+		}
+		cout << "\t";
+
+		if (parts[partindex].name) {
+			cout << parts[partindex].name->substr(3);
+		} else {
+			cout << ".";
+		}
+
+		cout << endl;
+	}
+
+	cout << "*-";
+	for (int i=1; i<pfeatures; i++) {
+		cout << "\t*-";
+	}
+	cout << endl;
+}
+
+
+
+//////////////////////////////
+//
+// printMeasureNumberInfo -- Prints a list of start times and stop times for 
+//    measures in the data (in units of quarter notes).
+//
+
+void printMeasureNumberInfo(vector<MeasureNumberInfo>& measures) {
+	int fcount = 3;
+	cout << "**num";
+	cout << "\t**start";
+	cout << "\t**stop";
+	cout << endl;
+	for (int i=0; i<(int)measures.size(); i++) {
+		cout << measures[i].number;
+		cout << "\t" << measures[i].start_time;
+		cout << "\t" << measures[i].stop_time;
+		cout << endl;
+	}
+	cout << "*-";
+	for (int i=0; i<fcount-1; i++) {
+		cout << "\t*-";
+	}
+	cout << endl;
+}
+
+
+
+//////////////////////////////
+//
+// extractMeasureNumberInfo --
+//
+
+void extractMeasureNumberInfo(vector<MeasureNumberInfo>& measures, HumdrumFile& infile) {
+	measures.clear();
+	measures.reserve(infile.getLineCount());
+	int number;
+	double start_time;
+	double stop_time;
+	for (int i=0; i<infile.getLineCount(); i++) {
+		if (!infile[i].isBarline()) {
+			continue;
+		}
+		number = infile.getMeasureNumber(i);
+		if (number < 0) {
+			continue;
+		}
+		start_time = infile[i].getDurationFromStart().getFloat();
+		stop_time = start_time + infile[i].getDurationToBarline().getFloat();
+		if (measures.size() > 0) {
+			// there could be barlines without numbers (such as at repeats),
+			// so revise the duration to skip unnumbered barlines.
+			measures.back().stop_time = start_time;
+		}
+		MeasureNumberInfo minfo;
+
+		if (measures.empty() && (start_time > 0.0)) {
+			// there is a pickup measure, so store information
+			// about it, using one less that the current measure number
+			minfo.number = number - 1;
+			minfo.start_time = 0.0;
+			minfo.stop_time = start_time;
+			measures.push_back(minfo);
+		}
+
+		minfo.number = number;
+		minfo.start_time = start_time;
+		minfo.stop_time = stop_time;
+		measures.push_back(minfo);
+	}
 }
 
 
@@ -64,25 +310,43 @@ int main(int argc, char** argv) {
 // printData --
 //
 
-void printData(vector<AccentFeatures>& data) {
-	int fcount = 19;          // Number of features in output data.
-	cout << "**kern";	  	  	  // Humdrum **kern pitch extracted from score.
-	cout << "\t**line";       // Line number in score (offset from 1).
+void printData(vector<AccentFeatures>& data, Options& options) {
+	int kernQ = options.getBoolean("kern");
+
+	int fcount = 21;          // Number of features in output data.
+	if (kernQ) {
+		fcount += 3;
+	}
+
+	if (kernQ) {
+		cout << "**kern";	  	  // Humdrum **kern pitch extracted from score.
+		cout << "\t";
+	}
+	cout << "**line";         // Line number in score (offset from 1).
 	cout << "\t**field";	     // Field number on line (offset from 1).
 	cout << "\t**track";      // Spine number on line (offset from 1).
+	cout << "\t**staff";      // Staff number on system (kern spine). 1 = top staff on system.
 	cout << "\t**subtrack";   // Subtrack information (currently a string, convert to integer later).
 	cout << "\t**start";      // Starting time of note in score as quarter notes from start of music.
 	cout << "\t**dur";        // Duration of the note in quarter notes.
 	cout << "\t**pitch";      // MIDI pitch number of note (60 = middle C)
 	cout << "\t**chord";      // Is note in chord; if so, then which note in chord?
-	cout << "\t**accent";     // Does note have an accent (^, ^^, z).
+	cout << "\t**accent";     // Does note have an accent (^).
+	cout << "\t**marcato";    // Does note have a marcato accent (^^).
+	cout << "\t**sforzando";  // Does note have a sforzando accent (z) (also check **dynam).
+	cout << "\t**tenuto";     // Does note have a tenuto (~).
+	cout << "\t**staccato";   // Does note have a staccato (', or `).
 	cout << "\t**trill";      // Does note have a trill (or mordent)
 	cout << "\t**slur_start"; // Does note have a slur start?
 	cout << "\t**slur_end";   // Does note have a slur end?
-	cout << "\t**pkern";      // **kern data for previous note (or chord)
+	if (kernQ) {
+		cout << "\t**pkern";   // **kern data for previous note (or chord)
+	}
 	cout << "\t**ppitch";     // MIDI note number of note that precedes.
 	cout << "\t**pstart";     // Starting time of the previous note in score (-1 means no previous note)
-	cout << "\t**nkern";      // **kern data for next note (or chord)
+	if (kernQ) {
+		cout << "\t**nkern";   // **kern data for next note (or chord)
+	}
 	cout << "\t**npitch";     // MIDI note number of note that follows.
 	cout << "\t**nstart";     // Starting time of the next note in score (-1 means no next note)
 	cout << endl;
@@ -90,16 +354,22 @@ void printData(vector<AccentFeatures>& data) {
 	for (int i=0; i<(int)data.size(); i++) {
 
 		// **kern
-		cout << data[i].text;
+		if (kernQ) {
+			cout << data[i].text;
+			cout << "\t";
+		}
 
 		// **line
-		cout << "\t" << data[i].token->getLineNumber();
+		cout << data[i].token->getLineNumber();
 
 		// **field
 		cout << "\t" << data[i].token->getFieldNumber();
 
 		// **track
 		cout << "\t" << data[i].token->getTrack();
+
+		// **staff
+		cout << "\t" << data[i].staff_num;
 
 		// **subtrack
 		cout << "\t" << data[i].token->getSpineInfo();
@@ -120,6 +390,18 @@ void printData(vector<AccentFeatures>& data) {
 		// **accent
 		cout << "\t" << (data[i].accent ? 1 : 0);
 
+		// **marcato
+		cout << "\t" << (data[i].marcato ? 1 : 0);
+
+		// **sforzando
+		cout << "\t" << (data[i].sforzando ? 1 : 0);
+
+		// **tenuto
+		cout << "\t" << (data[i].tenuto ? 1 : 0);
+
+		// **staccato
+		cout << "\t" << (data[i].staccato ? 1 : 0);
+
 		// **trill
 		cout << "\t" << (data[i].trill ? 1 : 0);
 
@@ -130,7 +412,9 @@ void printData(vector<AccentFeatures>& data) {
 		cout << "\t" << (data[i].slur_end ? 1 : 0);
 
 		// **pkern
-		cout << "\t" << (data[i].prevNote ? data[i].prevNote : 0);
+		if (kernQ) {
+			cout << "\t" << (data[i].prevNote ? data[i].prevNote : 0);
+		}
 
 		// **ppitch
 		cout << "\t" << (data[i].prevNote ? Convert::kernToMidiNoteNumber(data[i].prevNote) : 0);
@@ -144,7 +428,9 @@ void printData(vector<AccentFeatures>& data) {
 		}
 
 		// **nkern
-		cout << "\t" << (data[i].nextNote ? data[i].nextNote : 0);
+		if (kernQ) {
+			cout << "\t" << (data[i].nextNote ? data[i].nextNote : 0);
+		}
 
 		// **npitch
 		cout << "\t" << (data[i].nextNote ? Convert::kernToMidiNoteNumber(data[i].nextNote) : 0);
@@ -185,12 +471,25 @@ void extractFeatures(vector<AccentFeatures>& data) {
 			data[i].trill = true;
 		}
 
-		if (data[i].text.find("^") != string::npos) {
+		if (data[i].text.find("^^") != string::npos) {
+			data[i].marcato = true;
+		} else if (data[i].text.find("^") != string::npos) {
 			data[i].accent = true;
 		}
+
 		if (data[i].text.find("z") != string::npos) {
-			// sforzando
-			data[i].accent = true;
+			data[i].sforzando = true;
+		}
+
+		if (data[i].text.find("~") != string::npos) {
+			data[i].tenuto = true;
+		}
+
+		if (data[i].text.find("'") != string::npos) {
+			data[i].staccato = true;
+		} else if (data[i].text.find("`") != string::npos) {
+			// staccatissimo
+			data[i].staccato = true;
 		}
 
 		if (data[i].token->find("(") != string::npos) {
@@ -204,6 +503,7 @@ void extractFeatures(vector<AccentFeatures>& data) {
 		data[i].prevNote = getPreviousNote(data[i].token);
 	}
 }
+
 
 
 //////////////////////////////
@@ -261,6 +561,18 @@ HTp getPreviousNote(HTp starting) {
 
 void extractNotes(vector<AccentFeatures>& data, HumdrumFile& infile, 
 		Options& options) {
+
+	// partNumber: Part number (actually staff number), with "1" being the top
+	// staff in the system.
+	vector<HTp> kernspines = infile.getKernSpineStartList();
+	int trackcount = infile.getTrackCount();
+	vector<int> partNumber(trackcount + 1, 0);
+	for (int i=0; i<(int)kernspines.size(); i++) {
+		int track = kernspines[i]->getTrack();
+		int ktrack = (int)kernspines.size() - i;
+		partNumber[track] = ktrack;
+	}
+
 	data.clear();
 	data.reserve(infile.getLineCount() + infile.getMaxTrack() * 4);
 	for (int i=0; i<infile.getLineCount(); i++) {
@@ -301,6 +613,7 @@ void extractNotes(vector<AccentFeatures>& data, HumdrumFile& infile,
 				if (subcount > 1) {
 					af.chord_num++;
 				}
+				af.staff_num = partNumber[token->getTrack()];
 				data.push_back(af);
 			}
 		}
