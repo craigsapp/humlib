ef64663172373cebea3eb1ab572fd77c3d0c591d
Craig Stuart Sapp <craigsapp@gmail.com>
2019-12-16 10:08:12 -0800

    Move shed into cli directory.

--- cli/shed.cpp
+++ cli/shed.cpp
@@ -2,8 +2,8 @@
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sun Oct 13 12:36:47 PDT 2019
 // Last Modified: Sun Oct 13 12:36:50 PDT 2019
-// Filename:      humsar.cpp
-// URL:           https://github.com/craigsapp/humlib/blob/master/cli/humsar.cpp
+// Filename:      shed.cpp
+// URL:           https://github.com/craigsapp/humlib/blob/master/cli/shed.cpp
 // Syntax:        C++11
 // vim:           ts=3 noexpandtab nowrap
 //
@@ -12,7 +12,7 @@
 
 #include "humlib.h"
 
-STREAM_INTERFACE(Tool_humsar)
+STREAM_INTERFACE(Tool_shed)
 
 
 
