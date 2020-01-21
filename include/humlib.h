2a9deb3c13c7f0eee77faef546c36f3025f8871e
Craig Stuart Sapp <craigsapp@gmail.com>
2020-01-20 20:21:23 -0800

    Remove debug warning.

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Mon Jan 20 15:08:57 PST 2020
+// Last Modified: Mon Jan 20 20:18:39 PST 2020
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
--- src/humlib.cpp
+++ src/humlib.cpp
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Mon Jan 20 15:08:57 PST 2020
+// Last Modified: Mon Jan 20 20:18:39 PST 2020
 // Filename:      /include/humlib.cpp
 // URL:           https://github.com/craigsapp/humlib/blob/master/src/humlib.cpp
 // Syntax:        C++11
@@ -5464,7 +5464,6 @@ void GridSide::setHarmony(const string& token) {
 
 void GridSide::setDynamics(HTp token) {
 	if (m_dynamics) {
-		cerr << "WARHING: dynamic already exists here, deleting: " << m_dynamics << " and replacing with " << token << endl;
 		delete m_dynamics;
 		m_dynamics = NULL;
 	}
