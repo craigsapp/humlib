e038dfd2c95cd144227a19e1e83e40e48d363dfb
Craig Stuart Sapp <craigsapp@gmail.com>
2020-03-13 21:08:57 -0700

    Update autostem to complain about unbalanced beam marks.

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Sun Mar  1 23:01:25 PST 2020
+// Last Modified: Fri Mar 13 21:02:40 PDT 2020
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
@@ -4929,7 +4929,7 @@ class Tool_autostem : public HumTool {
 		void     initialize            (HumdrumFile& infile);
 		void      example              (void);
 		void      usage                (void);
-		void      autostem             (HumdrumFile& infile);
+		bool      autostem             (HumdrumFile& infile);
 		void      getClefInfo          (vector<vector<int> >& baseline,
 		                                HumdrumFile& infile);
 		void      addStem              (string& input, const string& piece);
@@ -4950,7 +4950,7 @@ class Tool_autostem : public HumTool {
 		                                vector<vector<int> >& baseline, int row, int col);
 		void      getMaxLayers         (vector<int>& maxlayer, vector<vector<int> >& voice,
 		                                HumdrumFile& infile);
-		void      assignStemDirections (vector<vector<int> >& stemdir,
+		bool      assignStemDirections (vector<vector<int> >& stemdir,
 		                                vector<vector<int> > & voice,
 		                                vector<vector<vector<int> > >& notepos,
 		                                HumdrumFile& infile);
@@ -4965,7 +4965,7 @@ class Tool_autostem : public HumTool {
 		                                vector<vector<int> >& stemdir);
 		void      setStemDirection     (HumdrumFile& infile, int row, int col,
 		                                int direction);
-		void      getBeamState         (vector<vector<string > >& beams,
+		bool      getBeamState         (vector<vector<string > >& beams,
 		                                HumdrumFile& infile);
 		void      countBeamStuff       (const string& token, int& start, int& stop,
 		                                int& flagr, int& flagl);
@@ -6139,6 +6139,7 @@ class Tool_mei2hum : public HumTool {
 		void   parseSectionScoreDef (xml_node scoreDef, HumNum starttime);
 		void   processPgHead        (xml_node pgHead, HumNum starttime);
 		void   processPgFoot        (xml_node pgFoot, HumNum starttime);
+		void   processKeySig        (mei_staffDef& staffinfo, xml_node keysig, HumNum starttime);
 		HumNum parseSection         (xml_node section, HumNum starttime);
 		HumNum parseApp             (xml_node app, HumNum starttime);
 		HumNum parseLem             (xml_node lem, HumNum starttime);
