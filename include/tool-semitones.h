78fdc80bc04fa51fcaf022fe79a4c997aac80499
Craig Stuart Sapp <craigsapp@gmail.com>
2020-11-15 07:58:36 -0800

    Update semitones tool.

--- include/tool-semitones.h
+++ include/tool-semitones.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Fri Nov 13 02:39:49 PST 2020
-// Last Modified: Fri Nov 13 02:39:39 PST 2020
+// Last Modified: Sat Nov 14 21:25:27 PST 2020
 // Filename:      tool-semitones.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/tool-semitones.h
 // Syntax:        C++11; humlib
@@ -27,21 +27,51 @@ class Tool_semitones : public HumTool {
 
 		bool  run              (HumdrumFileSet& infiles);
 		bool  run              (HumdrumFile& infile);
-		bool  run              (const string& indata, ostream& out);
-		bool  run              (HumdrumFile& infile, ostream& out);
+		bool  run              (const std::string& indata, std::ostream& out);
+		bool  run              (HumdrumFile& infile, std::ostream& out);
 
 	protected:
-		void   processFile(HumdrumFile& infile);
-		void   initialize(void);
-		void   analyzeLine(HumdrumFile& infile, int line);
-		int    processKernSpines(HumdrumFile& infile, int line, int start);
-		void   printTokens(const string& value, int count);
-		string getTwelveToneIntervalString(HTp token);
-		string getNextNoteAttack(HTp token);
+		void        processFile(HumdrumFile& infile);
+		void        initialize(void);
+		void        analyzeLine(HumdrumFile& infile, int line);
+		int         processKernSpines(HumdrumFile& infile, int line, int start, int kspine);
+		void        printTokens(const std::string& value, int count);
+		std::string getTwelveToneIntervalString(HTp token);
+		std::string getNextNoteAttack(HTp token);
+		void        markInterval(HTp token);
+		HTp         markNote(HTp token, bool markQ);
+		void        addMarker(HTp token);
+		void        showCount(void);
+		bool        filterData(HTp token);
+		std::vector<HTp> getTieGroup(HTp token);
+		HTp         getNextNote(HTp token);
+		bool        hasTieContinue(const string& value);
 
 	private:
-		bool m_cdata = false; // used **cdata (to display in VHV notation)
-		bool m_midi  = false; // give the MIDI note number rather than inteval.
+
+		bool        m_cdataQ      = false; // used **cdata (to display in VHV notation)
+		bool        m_downQ       = false; // mark/count notes in downward interval
+		bool        m_firstQ      = false; // mark only first note in interval
+		bool        m_leapQ       = false; // mark/count notes in leap motion
+		bool        m_midiQ       = false; // give the MIDI note number rather than inteval.
+		bool        m_noanalysisQ = false; // do not print analysis spines
+		bool        m_noinputQ    = false; // do not print input data
+		bool        m_nomarkQ     = false; // do not mark notes (just count intervals)
+		bool        m_norestsQ    = false; // ignore rests
+		bool        m_notiesQ     = false; // do not mark secondary tied notes
+		bool        m_repeatQ     = false; // make/count notes that repeat
+		bool        m_secondQ     = false; // mark only second note in interval
+		bool        m_stepQ       = false; // mark/count notes in stepwise motion
+		bool        m_upQ         = false; // mark/count notes in upward interval
+      bool        m_count       = false; // count the number of intervals being marked
+
+		int         m_markCount = 0;
+		int         m_leap      = 3;
+
+      std::string m_marker  = "@";
+		std::string m_color   = "red";
+		std::string m_include;
+		std::string m_exclude;
 
 };
 
--- src/humlib.cpp
+++ src/humlib.cpp
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Fri Nov 13 00:11:32 PST 2020
+// Last Modified: Sun Nov 15 07:51:46 PST 2020
 // Filename:      /include/humlib.cpp
 // URL:           https://github.com/craigsapp/humlib/blob/master/src/humlib.cpp
 // Syntax:        C++11
@@ -85317,9 +85317,26 @@ void Tool_scordatura::prepareTranspositionInterval(void) {
 //
 
 Tool_semitones::Tool_semitones(void) {
-	// initializations go here
+	define("1|first=b", "mark only the first note of intervals");
+	define("2|second=b", "mark only the second note of intervals");
+	define("A|O|no-analysis|no-output=b", "do not print analysis spines");
+	define("I|no-input=b", "do not print input data spines");
+	define("M|no-mark|no-marks=b", "do not mark notes");
+	define("R|no-rests=b", "ignore rests");
+	define("T|no-ties=b", "do not mark ties");
+	define("X|include|only=s", "include only **kern tokens with given pattern");
+	define("color=s:red", "mark color");
 	define("c|cdata=b", "store resulting data as **cdata (allowing display in VHV");
+	define("d|down=b", "highlight notes that that have a negative semitone interval");
+	define("j|jump=i:3", "starting interval defining leaps");
+	define("l|leap=b", "highlight notes that have leap motion");
+	define("mark=s:@", "mark character");
 	define("m|midi=b", "show MIDI note number for pitches");
+	define("n|count=b", "output count of intervals being marked");
+	define("r|same|repeat|repeated=b", "highlight notes that are repeated ");
+	define("s|step=b", "highlight notes that have step-wise motion");
+	define("u|up=b", "highlight notes that that have a positive semitone interval");
+	define("x|exclude=s", "exclude **kern tokens with given pattern");
 }
 
 
@@ -85377,8 +85394,34 @@ bool Tool_semitones::run(HumdrumFile& infile) {
 
 void Tool_semitones::initialize(void) {
 	// processing of options goes here
-	m_cdata = getBoolean("cdata");
-	m_midi  = getBoolean("midi");
+
+	m_cdataQ      = getBoolean("cdata");
+	m_count       = getBoolean("count");
+	m_downQ       = getBoolean("down");
+	m_firstQ      = getBoolean("first");
+	m_leapQ       = getBoolean("leap");
+	m_midiQ       = getBoolean("midi");
+	m_noanalysisQ = getBoolean("no-analysis");
+	m_noinputQ    = getBoolean("no-input");
+	m_nomarkQ     = getBoolean("no-marks");
+	m_notiesQ     = getBoolean("no-ties");
+	m_repeatQ     = getBoolean("repeat");
+	m_norestsQ    = getBoolean("no-rests");
+	m_secondQ     = getBoolean("second");
+	m_stepQ       = getBoolean("step");
+	m_upQ         = getBoolean("up");
+
+	m_leap        = getInteger("jump");
+
+	m_color       = getString("color");
+	m_exclude     = getString("exclude");
+	m_include     = getString("include");
+	m_marker      = getString("mark");
+
+	if (!m_firstQ && !m_secondQ) {
+		m_firstQ  = true;
+		m_secondQ = true;
+	}
 }
 
 
@@ -85389,10 +85432,58 @@ void Tool_semitones::initialize(void) {
 //
 
 void Tool_semitones::processFile(HumdrumFile& infile) {
+	m_markCount = 0;
 	for (int i=0; i<infile.getLineCount(); i++) {
 		analyzeLine(infile, i);
 	}
+	if (m_markCount > 0) {
+		m_humdrum_text << "!!!RDF**kern: ";
+		m_humdrum_text << m_marker;
+		m_humdrum_text << " = marked note";
+		if (getBoolean("color")) {
+			m_humdrum_text << ", color=" << m_color;
+		}
+		m_humdrum_text << '\n';
+	}
+	if (m_count) {
+		showCount();
+	}
 }
+
+
+
+//////////////////////////////
+//
+// Tool_semitones::showCount -- Give a count for the number of
+//     intervals that were marked.
+//
+
+void Tool_semitones::showCount(void) {
+	m_humdrum_text << "!!semitone_count: " << m_markCount;
+	if (m_repeatQ) {
+		m_humdrum_text << " REPEAT";
+	}
+	if (m_upQ) {
+		m_humdrum_text << " UP";
+	}
+	if (m_downQ) {
+		m_humdrum_text << " DOWN";
+	}
+	if (m_stepQ) {
+		m_humdrum_text << " STEP";
+	}
+	if (m_leapQ) {
+		m_humdrum_text << " LEAP";
+	}
+	if ((m_stepQ || m_leapQ) && (m_leap != 3)) {
+		m_humdrum_text << " JUMP:" << m_leap;
+	}
+	if (m_marker != "@") {
+		m_humdrum_text << " MARK:" << m_marker;
+	}
+	m_humdrum_text << '\n';
+}
+
 
 
 //////////////////////////////
@@ -85402,26 +85493,32 @@ void Tool_semitones::processFile(HumdrumFile& infile) {
 //
 
 void Tool_semitones::analyzeLine(HumdrumFile& infile, int line) {
+	int group = 0;
 	if (!infile[line].hasSpines()) {
 		m_humdrum_text << infile[line] << "\n";
 		return;
 	}
 	for (int i=0; i<infile[line].getFieldCount(); i++) {
 		HTp token = infile.token(line, i);
-		if (!token->isKern()) {
-			m_humdrum_text << token;
+		if (!m_noinputQ) {
+			if (!token->isKern()) {
+				m_humdrum_text << token;
+				if (i < infile[line].getFieldCount() - 1) {
+					m_humdrum_text << '\t';
+				}
+				continue;
+			}
+		}
+		i = processKernSpines(infile, line, i, group++);
+		if (!m_noinputQ) {
 			if (i < infile[line].getFieldCount() - 1) {
 				m_humdrum_text << '\t';
 			}
-         continue;
-		}
-		i = processKernSpines(infile, line, i);
-		if (i < infile[line].getFieldCount() - 1) {
-			m_humdrum_text << '\t';
 		}
 	}
 	m_humdrum_text << '\n';
 }
+
 
 
 //////////////////////////////
@@ -85429,7 +85526,7 @@ void Tool_semitones::analyzeLine(HumdrumFile& infile, int line) {
 // Tool_semitones::processKernSpine --
 //
 
-int Tool_semitones::processKernSpines(HumdrumFile& infile, int line, int start) {
+int Tool_semitones::processKernSpines(HumdrumFile& infile, int line, int start, int kspine) {
 	HTp token = infile.token(line, start);
 	if (!token->isKern()) {
 		return start;
@@ -85446,48 +85543,230 @@ int Tool_semitones::processKernSpines(HumdrumFile& infile, int line, int start) 
 		}
 		break;
 	}
-	for (int i=0; i<(int)toks.size(); i++) {
-		m_humdrum_text << toks[i] << '\t';
-	}
-	int csize = (int)toks.size();
-	if (!infile[line].isData()) {
-		if (infile[line].isLocalComment()) {
-			printTokens("!", csize);
-	 	} else if (infile[line].isInterpretation()) {
-			if (toks[0]->compare(0, 2, "**") == 0) { 
-				if (m_cdata) {
-					printTokens("**cdata", csize);
-				} else if (m_midi) {
-					printTokens("**mnn", csize);
-				} else {
-					printTokens("**tti", csize);
+
+	int toksize = (int)toks.size();
+
+	// calculate intervals/MIDI note numbers if appropriate
+	bool m_allQ = m_stepQ || m_leapQ || m_upQ || m_downQ || m_repeatQ;
+	vector<string> intervals(toksize);
+	if (infile[line].isData()) {
+		for (int i=0; i<toksize; i++) {
+			intervals[i] = getTwelveToneIntervalString(toks[i]);
+		}
+		if (m_allQ && !m_midiQ) {
+			for (int i=0; i<(int)intervals.size(); i++) {
+				if (intervals[i].empty()) {
+					continue;
 				}
-			} else {
-				for (int i=0; i<csize; i++) {
-					m_humdrum_text << toks[i];
-					if (i < csize - 1) {
-						m_humdrum_text << '\t';
-					}
+            if (!isdigit(intervals[i].back())) {
+					continue;
+				}
+				int value = stoi(intervals[i]);
+				if (m_upQ && m_stepQ && (value > 0) && (value < m_leap)) {
+					markInterval(toks[i]);
+				} else if (m_upQ && m_leapQ && (value > 0) && (value >= m_leap)) {
+					markInterval(toks[i]);
+				} else if (m_downQ && m_stepQ && (value < 0) && (value > -m_leap)) {
+					markInterval(toks[i]);
+				} else if (m_downQ && m_leapQ && (value < 0) && (value <= -m_leap)) {
+					markInterval(toks[i]);
+				} else if (m_stepQ && (value != 0) && (abs(value) < m_leap)) {
+					markInterval(toks[i]);
+				} else if (m_leapQ && (value != 0) && (abs(value) >= m_leap)) {
+					markInterval(toks[i]);
+				} else if (m_repeatQ && (value == 0)) {
+					markInterval(toks[i]);
+				} else if (m_upQ && (value > 0)) {
+					markInterval(toks[i]);
+				} else if (m_downQ && (value < 0)) {
+					markInterval(toks[i]);
 				}
 			}
-	 	} else if (infile[line].isBarline()) {
-			printTokens(*toks[0], csize);
-		} else {
-			cerr << "STRANGE ERROR " << toks[0] << endl;
-		}
-		return start + csize - 1;
-	}
-	// print twelve-tone analyses.
-	string value;
-	for (int i=0; i<csize; i++) {
-		value = getTwelveToneIntervalString(toks[i]);
-		m_humdrum_text << value;
-		if (i < csize - 1) {
-			m_humdrum_text << '\t';
 		}
 	}
 
-	return start + csize - 1;
+	// print the **kern fields
+	if (!m_noinputQ) {
+		for (int i=0; i<toksize; i++) {
+			m_humdrum_text << toks[i];
+			if (i < toksize - 1) {
+				m_humdrum_text << '\t';
+			}
+		}
+	}
+
+	// then print the parallel analysis fields
+
+	if (!m_noanalysisQ) {
+		if (!m_noinputQ) {
+			m_humdrum_text << '\t';
+		} else if (m_noinputQ && (kspine != 0)) {
+			m_humdrum_text << '\t';
+		}
+		if (!infile[line].isData()) {
+			if (infile[line].isLocalComment()) {
+				printTokens("!", toksize);
+	 		} else if (infile[line].isInterpretation()) {
+				if (toks[0]->compare(0, 2, "**") == 0) { 
+					if (m_cdataQ) {
+						printTokens("**cdata", toksize);
+					} else if (m_midiQ) {
+						printTokens("**mnn", toksize);
+					} else {
+						printTokens("**tti", toksize);
+					}
+				} else {
+					for (int i=0; i<toksize; i++) {
+						m_humdrum_text << toks[i];
+						if (i < toksize - 1) {
+							m_humdrum_text << '\t';
+						}
+					}
+				}
+	 		} else if (infile[line].isBarline()) {
+				printTokens(*toks[0], toksize);
+			} else {
+				cerr << "STRANGE ERROR " << toks[0] << endl;
+			}
+			return start + toksize - 1;
+		}
+		// print twelve-tone analyses.
+		string value;
+		for (int i=0; i<toksize; i++) {
+			value = getTwelveToneIntervalString(toks[i]);
+			m_humdrum_text << value;
+			if (i < toksize - 1) {
+				m_humdrum_text << '\t';
+			}
+		}
+	}
+
+	return start + toksize - 1;
+}
+
+
+
+//////////////////////////////
+//
+// Tool_semitones::markInterval -- mark the current note, any notes tied
+//    after it, and then the next note and any tied notes attached to
+//    that note.
+//
+
+void Tool_semitones::markInterval(HTp token) {
+	if (!token->isData()) {
+		return;
+	}
+	if (!token->isKern()) {
+		return;
+	}
+	if (token->isNull()) {
+		return;
+	}
+	if (token->isRest()) {
+		return;
+	}
+	if (token->isUnpitched()) {
+		return;
+	}
+	m_markCount++;
+	token = markNote(token, m_firstQ);
+	if (m_firstQ && !m_secondQ) {
+		return;
+	}
+	// find next note
+	HTp current = token->getNextToken();
+	while (current) {
+		if (!current->isData()) {
+			current = current->getNextToken();
+			continue;
+		}
+		if (current->isNull()) {
+			current = current->getNextToken();
+			continue;
+		}
+		markNote(current, m_secondQ);
+		break;
+	}
+}
+
+
+
+//////////////////////////////
+//
+// Tool_semitones::markNote -- make note and any tied notes after it.
+//      Return the last note of a tied note (or the note if no tied notes
+//      after it).
+//
+
+HTp Tool_semitones::markNote(HTp token, bool markQ) {
+	string subtok = token->getSubtoken(0);
+	bool hasTieEnd = false;
+	if (subtok.find('_') != string::npos) {
+		hasTieEnd = true;
+	} else if (subtok.find(']') != string::npos) {
+		hasTieEnd = true;
+	}
+
+	if (!(hasTieEnd && m_notiesQ)) {
+		if (markQ) {
+			addMarker(token);
+		}
+	}
+
+	bool hasTie = false;
+	if (subtok.find('[') != string::npos) {
+		hasTie = true;
+	} else if (subtok.find('_') != string::npos) {
+		hasTie = true;
+	}
+
+	if (!hasTie) {
+		return token;
+	}
+	HTp current = token->getNextToken();
+	while (current) {
+		if (!current->isData()) {
+			current = current->getNextToken();
+			continue;
+		}
+		if (current->isNull()) {
+			current = current->getNextToken();
+			continue;
+		}
+		subtok = current->getSubtoken(0);
+		bool hasTie = false;
+		if (subtok.find('[') != string::npos) {
+			hasTie = true;
+		} else if (subtok.find('_') != string::npos) {
+			hasTie = true;
+		}
+		if (!hasTie) {
+			if (subtok.find(']') != string::npos) {
+				markNote(current, markQ);
+			}
+			return current;
+		} else {
+			return markNote(current, markQ);
+		}
+		break;
+	}
+	return NULL;
+}
+
+
+
+//////////////////////////////
+//
+// Tool_semitones::addMarker --
+//
+
+void Tool_semitones::addMarker(HTp token) {
+	if (!m_nomarkQ) {
+		string contents = m_marker;
+		contents += token->getText();
+		token->setText(contents);
+	}
 }
 
 
@@ -85518,9 +85797,22 @@ string Tool_semitones::getTwelveToneIntervalString(HTp token) {
 		return ".";
 	}
 	if (token->isRest()) {
-		if (m_midi) {
+		if (m_midiQ) {
 			return "r";
 		} else {
+			return ".";
+		}
+	}
+	if (token->isUnpitched()) {
+		if (m_midiQ) {
+			return "R";
+		} else {
+			return ".";
+		}
+	}
+	if ((m_include.size() > 0) || (m_exclude.size() > 0)) {
+		int status = filterData(token);
+		if (!status) {
 			return ".";
 		}
 	}
@@ -85533,7 +85825,7 @@ string Tool_semitones::getTwelveToneIntervalString(HTp token) {
 	}
 	int value = Convert::kernToMidiNoteNumber(tok);
 
-	if (m_midi) {
+	if (m_midiQ) {
 		string output;
 		output = to_string(value);
 		return output;
@@ -85574,7 +85866,15 @@ string Tool_semitones::getNextNoteAttack(HTp token) {
 			continue;
 		}
 		if (current->isRest()) {
-			return "r";
+			if (!m_norestsQ) {
+				return "r";
+			} else {
+				current = current->getNextToken();
+				continue;
+			}
+		}
+		if (current->isUnpitched()) {
+			return "R";
 		}
 		string tok = current->getSubtoken(0);
 		if (tok.find(']') != string::npos) {
@@ -85596,6 +85896,116 @@ string Tool_semitones::getNextNoteAttack(HTp token) {
 	}
 	// Some other strange problem.
 	return ".";
+}
+
+
+
+//////////////////////////////
+//
+// Tool_semitones::filterData -- select or deselect an interval based
+//    on regular expression pattern.  Return true if the note should
+//    be kept; otherwise, return false.
+//
+
+bool Tool_semitones::filterData(HTp token) {
+	vector<HTp> toks = getTieGroup(token);
+	HumRegex hre;
+	if (!m_exclude.empty()) {
+		for (int i=0; i<(int)toks.size(); i++) {
+			if (hre.search(toks[i], m_exclude)) {
+				return false;
+			}
+		}
+		return true;
+	} else if (!m_include.empty()) {
+		for (int i=0; i<(int)toks.size(); i++) {
+			if (hre.search(toks[i], m_include)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	return false;
+}
+
+
+
+//////////////////////////////
+//
+// Tool_semitones::getTieGroup --
+//
+
+vector<HTp> Tool_semitones::getTieGroup(HTp token) {
+	vector<HTp> output;
+	if (!token) {
+		return output;
+	}
+	if (token->isNull()) {
+		return output;
+	}
+	if (!token->isData()) {
+		return output;
+	}
+	output.push_back(token);
+	if (token->isRest()) {
+		return output;
+	}
+	string subtok = token->getSubtoken(0);
+	bool continues = hasTieContinue(subtok);
+	HTp current = token;
+	while (continues) {
+		current = getNextNote(current);
+		if (!current) {
+			break;
+		}
+		string subtok = current->getSubtoken(0);
+		if (subtok.find(']') != string::npos) {
+			output.push_back(current);
+			break;
+		}
+		continues = hasTieContinue(subtok);
+	}
+	return output;
+}
+
+
+
+//////////////////////////////
+//
+// Tool_semitones::hasTieContinue --
+//
+
+bool Tool_semitones::hasTieContinue(const string& value) {
+	if (value.find('_') != string::npos) {
+		return true;
+	}
+	if (value.find('[') != string::npos) {
+		return true;
+	}
+	return false;
+}
+
+
+
+//////////////////////////////
+//
+// getNextNote --
+//
+
+HTp Tool_semitones::getNextNote(HTp token) {
+	HTp current = token->getNextToken();
+	while (current) {
+		if (!current->isData()) {
+			current = current->getNextToken();
+			continue;
+		}
+		if (current->isNull()) {
+			current = current->getNextToken();
+			continue;
+		}
+		break;
+	}
+	return current;
 }
 
 
