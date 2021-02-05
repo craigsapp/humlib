a68216fb94c96d9e096c54a758a52edc6792d3b6
Craig Stuart Sapp <craigsapp@gmail.com>
2021-02-05 11:05:01 -0800

    Add initial fixps tool.

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Sun Jan 31 21:47:47 PST 2021
+// Last Modified: Wed Feb  3 23:31:08 PST 2021
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
@@ -6164,6 +6164,30 @@ class Tool_filter : public HumTool {
 	private:
 		string   m_variant;        // used with -v option.
 		bool     m_debugQ = false; // used with --debug option
+
+};
+
+
+class Tool_fixps : public HumTool {
+	public:
+		         Tool_fixps         (void);
+		        ~Tool_fixps         () {};
+
+		bool     run                (HumdrumFileSet& infiles);
+		bool     run                (HumdrumFile& infile);
+		bool     run                (const string& indata, ostream& out);
+		bool     run                (HumdrumFile& infile, ostream& out);
+
+	protected:
+		void     initialize         (HumdrumFile& infile);
+		void     processFile        (HumdrumFile& infile);
+		void     markEmptyVoices    (HumdrumFile& infile);
+		void     removeEmpties      (vector<vector<HTp>>& newlist, HumdrumFile& infile);
+		void     removeDuplicateDynamics(HumdrumFile& infile);
+		void     outputNewSpining   (vector<vector<HTp>>& newlist, HumdrumFile& infile);
+		void     printNewManipulator(HumdrumFile& infile, vector<vector<HTp>>& newlist, int line);
+
+	private:
 
 };
 
