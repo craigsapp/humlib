a33cd71278d27e808b047b5946688fe15d2445f4
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-27 19:44:59 +0100

    Refactor with classes

--- include/tool-figuredbass.h
+++ include/tool-figuredbass.h
@@ -9,6 +9,31 @@ namespace hum {
 
 // START_MERGE
 
+class FiguredBassNumber {
+	public:
+		int number;
+		string accidentals;
+		FiguredBassNumber(int num, string accid);
+		string toString(bool nonCompoundIntervalsQ, bool noAccidentalsQ);
+};
+
+class FiguredBassSlice {
+	public:
+		int lineIndex;
+		vector<FiguredBassNumber*> numbers;
+		FiguredBassSlice(int index);
+		void add(FiguredBassNumber* number);
+		string toString(bool nonCompoundIntervalsQ, bool noAccidentalsQ);
+};
+
+class FiguredBassTrack {
+	public:
+		int voiceIndex;
+		vector<FiguredBassSlice*> slices;
+		FiguredBassTrack(int index);
+		void add(FiguredBassSlice* slice);
+};
+
 class Tool_figuredbass : public HumTool {
 
 	public:
@@ -19,6 +44,10 @@ class Tool_figuredbass : public HumTool {
 		bool run (HumdrumFile& infile);
 		bool run (const string& indata, ostream& out);
 		bool run (HumdrumFile& infile, ostream& out);
+
+		vector<string> getTrackData(FiguredBassTrack* track, int lineCount, bool nonCompoundIntervalsQ, bool noAccidentalsQ);
+
+		FiguredBassNumber* createFiguredBassNumber(NoteCell* base, NoteCell* target);
 
 	// protected:
 
--- src/tool-figuredbass.cpp
+++ src/tool-figuredbass.cpp
@@ -51,54 +51,113 @@ bool Tool_figuredbass::run(HumdrumFile &infile) {
 
 	NoteGrid grid(infile);
 
-	vector<string> data;
-	data.resize(infile.getLineCount());
+	vector<FiguredBassTrack*> data;
 
 	vector<HTp> kernspines = infile.getKernSpineStartList();
 
 	int baseVoiceIndex = baseQ;
+	FiguredBassTrack* track = new FiguredBassTrack(baseVoiceIndex);
+	data.push_back(track);
 
 	for (int i=0; i<(int)grid.getSliceCount(); i++) {
-		NoteCell* baseCell = grid.cell(baseVoiceIndex, i);
+		NoteCell* baseCell = grid.cell(track->voiceIndex, i);
+		FiguredBassSlice* slice = new FiguredBassSlice(baseCell->getLineIndex());
+		track->add(slice);
 		for (int j=0; j<(int)grid.getVoiceCount(); j++) {
 			if(j == baseVoiceIndex) {
 				continue;
 			}
 			NoteCell* targetCell = grid.cell(j, i);
-			int basePitch = baseCell->getSgnDiatonicPitch();
-			int targetPitch = targetCell->getSgnDiatonicPitch();
-			int num = (basePitch == 0 || targetPitch == 0) ? 0 : abs(abs(targetPitch) - abs(basePitch)) + 1;
-			if(num > 9 && !nonCompoundIntervalsQ) {
-				num = num % 7;
-			}
-			if(num > 0) {
-				string accid = "";
-				if (!noAccidentalsQ) {
-					// TODO improve for double sharps and double flats
-					if(targetCell->getSgnKernPitch().find("-") != string::npos) {
-						accid = '-';
-					} else if(targetCell->getSgnKernPitch().find("#") != string::npos) {
-						accid = '#';
-					}
-				}
-				
-				data[baseCell->getLineIndex()] = accid + to_string(num) + " " + data[baseCell->getLineIndex()];
-			}
+
+
+			FiguredBassNumber* number = createFiguredBassNumber(baseCell, targetCell);
+			slice->add(number);
 		}
 	}
 
-	if(baseVoiceIndex < grid.getVoiceCount()) {
-		int trackIndex = kernspines[baseVoiceIndex + 1]->getTrack();
-		infile.insertDataSpineBefore(trackIndex, data, ".", "**fb");
-	} else {
-		// TODO segmentation fault
-		infile.appendDataSpine(data, ".", "**fb");
+ 	for (FiguredBassTrack* track: data) {
+		if(baseVoiceIndex < grid.getVoiceCount()) {
+			int trackIndex = kernspines[baseVoiceIndex + 1]->getTrack();
+			infile.insertDataSpineBefore(trackIndex, getTrackData(track, infile.getLineCount(), nonCompoundIntervalsQ, noAccidentalsQ), ".", "**fb");
+		} else {
+			// TODO segmentation fault
+			infile.appendDataSpine(getTrackData(track, infile.getLineCount(), nonCompoundIntervalsQ, noAccidentalsQ), ".", "**fb");
+		}
 	}
 
-
 	return true;
-
 };
+
+vector<string> Tool_figuredbass::getTrackData(FiguredBassTrack* track, int lineCount, bool nonCompoundIntervalsQ, bool noAccidentalsQ) {
+	vector<string> trackData;
+	trackData.resize(lineCount);
+
+ 	for (FiguredBassSlice* slice: track->slices) {
+		trackData[slice->lineIndex] = slice->toString(nonCompoundIntervalsQ, noAccidentalsQ);
+	}
+
+	return trackData;
+};
+
+FiguredBassNumber* Tool_figuredbass::createFiguredBassNumber(NoteCell* base, NoteCell* target) {
+
+	int basePitch = base->getSgnDiatonicPitch();
+	int targetPitch = target->getSgnDiatonicPitch();
+	int num = (basePitch == 0 || targetPitch == 0) ? 0 : abs(abs(targetPitch) - abs(basePitch)) + 1;
+	string accid = "";
+	if(target->getSgnKernPitch().find("-") != string::npos) {
+		accid = '-';
+	} else if(target->getSgnKernPitch().find("#") != string::npos) {
+		accid = '#';
+	}
+
+	FiguredBassNumber* number = new FiguredBassNumber(num, accid);
+
+	return number;
+};
+
+FiguredBassTrack::FiguredBassTrack(int index) {
+	voiceIndex = index;
+};
+
+void FiguredBassTrack::add(FiguredBassSlice* slice) {
+	slices.push_back(slice);
+};
+
+FiguredBassSlice::FiguredBassSlice(int index) {
+	lineIndex = index;
+};
+
+void FiguredBassSlice::add(FiguredBassNumber* number) {
+	numbers.insert(numbers.begin(), number);
+	// numbers.push_back(number);
+};
+
+string FiguredBassSlice::toString(bool nonCompoundIntervalsQ, bool noAccidentalsQ) {
+	string str = "";
+	bool first = true;
+	for (FiguredBassNumber* number: numbers) {
+		string num = number->toString(nonCompoundIntervalsQ, noAccidentalsQ);
+		if(num.length() > 0) {
+			if (!first) str += " ";
+			first = false;
+			str += number->toString(nonCompoundIntervalsQ, noAccidentalsQ);
+		}
+	}
+	return str;
+};
+
+FiguredBassNumber::FiguredBassNumber(int num, string accid) {
+	number = num;
+	accidentals = accid;
+};
+
+string FiguredBassNumber::toString(bool nonCompoundIntervalsQ, bool noAccidentalsQ) {
+	int num = (number > 9 && !nonCompoundIntervalsQ) ? number % 7 : number;
+	string accid = (!noAccidentalsQ) ? accidentals : "";
+	return num > 0 ? to_string(num) + accid : "";
+};
+
 
 
 // END_MERGE
