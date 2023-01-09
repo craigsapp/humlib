2970ee5e83b405aa110fc653be3b27de76ee2f0b
Craig Stuart Sapp <craigsapp@gmail.com>
2023-01-09 10:19:08 -0800

    Sort list of filters alphabetically and separate list of filters with alias afterwards.

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Mon Jan  9 09:45:43 PST 2023
+// Last Modified: Mon Jan  9 10:19:01 PST 2023
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
