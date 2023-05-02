a90ceaacf17b776936b8f99d2170cafc65baa604
Craig Stuart Sapp <craigsapp@gmail.com>
2023-05-02 07:49:24 -0700

    Add ordergps tool files for real.

---
+++ cli/ordergps.cpp
@@ -0,0 +1,18 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Mon May  1 07:46:30 PDT 2023
+// Last Modified: Mon May  1 07:46:34 PDT 2023
+// Filename:      ordergps.cpp
+// URL:           https://github.com/craigsapp/humlib/blob/master/cli/ordergps.cpp
+// Syntax:        C++11
+// vim:           ts=3 noexpandtab nowrap
+//
+// Description:   Reverse order of subspines.
+//
+
+#include "humlib.h"
+
+STREAM_INTERFACE(Tool_ordergps)
+
+
+
---
+++ include/tool-ordergps.h
@@ -0,0 +1,59 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Mon May  1 07:19:59 PDT 2023
+// Last Modified: Mon May  1 07:20:02 PDT 2023
+// Filename:      tool-ordergps.h
+// URL:           https://github.com/craigsapp/humlib/blob/master/include/tool-ordergps.h
+// Syntax:        C++11; humlib
+// vim:           ts=3 noexpandtab
+//
+// Description:   Make *group, *part, and *staff interpretations adjacent and in
+//                that order.  Only looking before the first data line.
+//
+
+#ifndef _TOOL_ORDERGPS_H
+#define _TOOL_ORDERGPS_H
+
+#include "HumTool.h"
+#include "HumdrumFile.h"
+
+namespace hum {
+
+// START_MERGE
+
+class Tool_ordergps : public HumTool {
+	public:
+		      Tool_ordergps  (void);
+		     ~Tool_ordergps  () {};
+
+		bool  run            (HumdrumFileSet& infiles);
+		bool  run            (HumdrumFile& infile);
+		bool  run            (const string& indata, ostream& out);
+		bool  run            (HumdrumFile& infile, ostream& out);
+
+	protected:
+		void  initialize     (void);
+		void  processFile    (HumdrumFile& infile);
+		void  printStaffLine (HumdrumFile& infile);
+		void  printFile      (HumdrumFile& infile, int gindex, int pindex, int sindex);
+		void  printFileTop   (HumdrumFile& infile, int gindex, int pindex, int sindex);
+
+	private:
+		bool m_emptyQ   = false;
+		bool m_fileQ    = false;
+		bool m_listQ    = false;
+		bool m_problemQ = false;
+		bool m_reverseQ = false;
+		bool m_staffQ   = false;
+		bool m_topQ     = false;
+
+};
+
+// END_MERGE
+
+} // end namespace hum
+
+#endif /* _TOOL_ORDERGPS_H */
+
+
+
