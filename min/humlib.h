1a014183ecaa016316988eaabb7d8596077fefbd
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-12-05 12:01:18 +0100

    Add missing humlib build

--- min/humlib.h
+++ min/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Mo  4 Dez 2023 11:23:27 CET
+// Last Modified: Di  5 Dez 2023 11:57:39 CET
 // Filename:      min/humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/min/humlib.h
 // Syntax:        C++11
