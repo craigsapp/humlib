826b3312f9835803e4ff0d9fefa6d0e01f6f8ec4
Craig Sapp <craig@Craigs-MacBook-Air.local>
2026-02-23 14:08:41 -0800

    Add text cli

---
+++ cli/text.cpp
@@ -0,0 +1,18 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Sat Sep  2 16:14:22 CEST 2023
+// Last Modified: Sat Sep  2 16:14:24 CEST 2023
+// Filename:      cli/text.cpp
+// URL:           https://github.com/craigsapp/humlib/blob/master/cli/text.cpp
+// Syntax:        C++11
+// vim:           ts=3 noexpandtab nowrap
+//
+// Description:   Process **text spines.
+//
+
+#include "humlib.h"
+
+STREAM_INTERFACE(Tool_text)
+
+
+
