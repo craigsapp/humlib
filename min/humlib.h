7359a79fe75feec6e44e941761c6a4e0f90d66a2
Craig Stuart Sapp <craigsapp@gmail.com>
2026-08-18 13:43:41 -0700

    rename esac2hum to esac2humx to avoid name collision with old program

--- min/humlib.h
+++ min/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Tue Aug 18 01:30:46 PDT 2026
+// Last Modified: Tue Aug 18 13:40:26 PDT 2026
 // Filename:      min/humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/min/humlib.h
 // Syntax:        C++11
