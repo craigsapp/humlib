0f78eb2671b3548bc25b3a11c64b63ec06d772e2
Alexander Morgan <alexanderpmorgan@gmail.com>
2026-09-19 08:32:04 +0200

    WIP: finish Tsg16064a

--- src/tool-autocadence.cpp
+++ src/tool-autocadence.cpp
@@ -3544,6 +3544,7 @@ void Tool_autocadence::prepareCadenceLabels(void) {
 	m_cadenceLabels.emplace("Ct",   "Evaded Clausula Vera");
 	m_cadenceLabels.emplace("CTp",  "Evaded Clausula Vera");  // TODO: check this, perhaps it shouldn't be evaded
 	m_cadenceLabels.emplace("CTpt", "Evaded Clausula Vera");  // TODO: check this, perhaps it shouldn't be evaded
+	m_cadenceLabels.emplace("ctz",  "Evaded Clausula Vera");
 	m_cadenceLabels.emplace("bxy",  "Abandoned Authentic");
 	m_cadenceLabels.emplace("by",   "Abandoned Authentic");
 	m_cadenceLabels.emplace("byz",  "Abandoned Authentic");
